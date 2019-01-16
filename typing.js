4b1a512c07c9dc5f9a92ba6038032066b41bd386
chloebyunn <chloebyunn@gmail.com>
2019-01-16 15:46:42 -0500

    bugs responsiveness and animations

---
+++ typing.js
@@ -0,0 +1,14 @@
+var options = {
+    strings: ["development.", "design.", "product strategy.", "food!"],
+    typeSpeed: 70, 
+    loop: true,
+    loopCount: Infinity,
+    backSpeed: 50,
+    smartBackspace: true,
+    fadeOutClass: 'typed-fade-out',
+    // startDelay: 2110,
+    backDelay: 1400,
+    // showCursor: 
+  }
+  
+  var typed = new Typed(".secondary-highlight", options);
